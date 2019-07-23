d91565e14f188cef2d5ae7e88528b4f6781d0682
Karim <karim.vally@outlook.com>
2019-07-23 20:12:38 +0100

    Added simple result base interface and implementation

---
+++ ResultBase/Interfaces/IResult.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace ResultBase.Interfaces
+{
+    public interface IResult<T, TS>
+        where TS : Enum
+    {
+        TS Status { get; set; }
+    }
+}
---
+++ ResultBase/Result.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using ResultBase.Interfaces;
+
+namespace ResultBase
+{
+    public class Result<T, TS> : IResult<T, TS>
+        where TS : Enum
+    {
+        public TS Status { get; set; }
+    }
+}
