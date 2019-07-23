361c6e2276994a1a7bd98349d22a6e2af872e5d1
Karim <karim.vally@outlook.com>
2019-07-23 20:15:03 +0100

    Created ItemResult implementation for result

---
+++ ResultBase/Interfaces/IItemResult.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace ResultBase.Interfaces
+{
+    public interface IItemResult<T, TS> : IResult<T, TS>
+        where TS : Enum
+    {
+        T Data { get; set; }
+    }
+}
---
+++ ResultBase/ItemResult.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using ResultBase.Interfaces;
+
+namespace ResultBase
+{
+    public class ItemResult<T, TS> : IItemResult<T, TS>
+        where TS : Enum
+    {
+        public T Data { get; set; }
+        public TS Status { get; set; }
+    }
+}
