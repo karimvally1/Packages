8baad95cefaaf677e05121d85837d12fac7d0f32
Karim <karim.vally@outlook.com>
2019-07-25 20:00:39 +0100

    Added extensions to seperate casting

---
+++ ResponseBase/Extensions/ResponseExtensions.cs
@@ -0,0 +1,23 @@
+﻿using ResponseBase.Interfaces;
+
+namespace ResponseBase.Extensions
+{
+    public static class ResponseExtensions
+    {
+        public static IErrorResponse<T> ToErrorResponse<T>(this IResponse<T> response)
+        {
+            return response as IErrorResponse<T>;
+        }
+
+        public static IItemResponse<T> ToItemResponse<T>(this IResponse<T> response)
+        {
+            return response as IItemResponse<T>;
+        }
+
+        public static IPagedResponse<T> ToPagedResponse<T>(this IResponse<T> response)
+        {
+            return response as IPagedResponse<T>;
+        }
+
+    }
+}
